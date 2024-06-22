0ffe6403b2312275911b3fe29144708a21a7696c
Quoc Thao <quocthao23061998.tg@gmail.com>
2024-06-22 14:51:44 +0700

    Initial Commit

---
+++ BarCodeScanner/BarCodeScannerApp.swift
@@ -0,0 +1,17 @@
+//
+//  BarCodeScannerApp.swift
+//  BarCodeScanner
+//
+//  Created by Jin on 22/6/24.
+//
+
+import SwiftUI
+
+@main
+struct BarCodeScannerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BarCodeScanner/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  BarCodeScanner
+//
+//  Created by Jin on 22/6/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
